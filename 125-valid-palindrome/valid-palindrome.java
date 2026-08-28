d365235db5228648de4e7b1823c0dfedbbec7405
shreysri05 <2k24.csai1c.2410177@gmail.com>
2026-08-28 08:14:28 +0530

    Time: 3 ms (57.43%) | Memory: 44.5 MB (54.08%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+   int i=0,j=s.length()-1;
+   s=s.toLowerCase();
+   while(i<j){
+    if(!Character.isLetterOrDigit(s.charAt(i))) {i++; continue;}
+    if(!Character.isLetterOrDigit(s.charAt(j))) {j--; continue;}
+    if (s.charAt(i)!=s.charAt(j)) return false;
+    else {i++;
+         j--;}
+    
+   } return true;
+        
+    }
+}
