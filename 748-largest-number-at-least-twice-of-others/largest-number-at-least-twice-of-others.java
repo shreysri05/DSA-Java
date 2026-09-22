44ffc110747be518823b24285ed237c7c8385726
shreysri05 <2k24.csai1c.2410177@gmail.com>
2026-09-22 06:16:02 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (71.75%) - LeetSync

---
+++ 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int dominantIndex(int[] nums) {
+        int max=nums[0];
+        int index=0;
+        for(int i =1;i<nums.length;i++){
+          if (nums[i]>max){max=nums[i]; index=i;}
+
+        }
+        for(int i =0;i<nums.length;i++){
+            if (index==i||nums[i]*2<=max) continue;
+            return -1;
+            
+        }
+        return index;
+    }
+}
